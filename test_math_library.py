966e86bbc2b85599305030098f17e4439341a664
Eirik Haustveit <eirik@la2kta.net>
2019-06-29 19:33:36 +0200

    Added unit test for math library

---
+++ test_math_library.py
@@ -0,0 +1,14 @@
+"""
+Unit test for math_library.py
+"""
+
+import math_library
+
+
+class TestMathLibrary:
+
+    def test_add(self):
+        assert 4 == math_library.add(2, 2)
+
+    def test_sub(self):
+        assert 2 == math_library.sub(4, 2)
